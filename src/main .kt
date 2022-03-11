a5e95565608cfa79cf75c008152dbfe7b1d3fcc3
beatriceanena <anenabeattrice@gmail.com>
2022-03-11 20:15:25 +0300

    first commit

---
+++ src/main .kt
@@ -0,0 +1,33 @@
+//question 1
+fun main(){
+    colors("black","blue","yellow","red")
+    capitalcities(arrayOf("harare","mumbai", "dodoma","jakarta"))
+    num(arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62))
+    var t=mynames("anena","lilian","brenda")
+println(t.contentToString())
+}
+fun colors(e:String, f:String, g:String, h:String){
+    var output= arrayOf(e,f,g,h)
+    println(output.contentToString())
+}
+//question 2
+fun capitalcities(cities:Array<String>){
+    cities.forEach{t ->
+        println(t.capitalize())git initgit init
+    }
+
+}
+//question 3
+fun num(numbers:Array<Int>){
+    var sum= numbers[1]+numbers[4]
+    println(sum)
+    var index= numbers.indexOf(158)
+    println(index)
+    var sorted= (numbers.sortedArray())
+    println(sorted.contentToString())
+}
+//question 4
+fun mynames(a:String, b:String, c:String):Array<String>{
+    var p =arrayOf(a,b,c)
+    return p
+}
